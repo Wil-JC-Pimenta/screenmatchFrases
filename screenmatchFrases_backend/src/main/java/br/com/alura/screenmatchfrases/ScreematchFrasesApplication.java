e3a2fbeb5c3dbcfb95f2cbcfeb5577cf5098716c
Wilker Junio <125089895+Wil-JC-Pimenta@users.noreply.github.com>
2024-06-16 16:20:51 -0300

    Add screenmatchFrases_Backend

---
+++ screenmatchFrases_backend/src/main/java/br/com/alura/screenmatchfrases/ScreematchFrasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatchfrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreematchFrasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreematchFrasesApplication.class, args);
+    }
+
+}
